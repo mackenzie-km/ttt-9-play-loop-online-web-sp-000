ff0fa9e2ea81d2a8443b64e9fdb30eb831188da1
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-19 18:26:12 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,6 @@
-# Helper Methods
+index = 0 
+
+#displays the board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,32 +9,62 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+#converts the input to index
+def input_to_index(user_input = 0)
+  index = (user_input.to_i - 1)
 end
 
-def move(board, index, current_player = "X")
-  board[index] = current_player
+#marks X or O based on user's input
+def move(board, index, character = "X")
+  if character == "X"
+    board[index] = "X"
+  else character == "O"
+    board[index] = "O"
+end
 end
 
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
+#helper method to see if it is open
+def position_taken?(board, index)
+  if board[index] == "X"
+    true
+  elsif board[index] == "O"
+    true
   else
+    false
+  end
+end
+
+#validates code to check that it is a valid move
+def valid_move?(board, index)
+  if (index.between?(0,8) == true) && (position_taken?(board, index) == false)
+    return true 
+  else
+    return false
+  end
+end
+
+#combines helper methods to take a turn
+def turn(board)
+  character = "X"
+puts "Please enter 1-9:"
+user_input = gets.strip
+user_input = user_input.to_i
+index = input_to_index(user_input)
+ if valid_move?(board, index) == true
+   move(board, index, character = "X")
+   display_board(board)
+  else 
     turn(board)
   end
 end
 
-# Define your play method below
+#the meat!
+def play(board)
+ i = 0
+ while i < 9
+ i += 1
+  turn(board)
+  end
+  puts "Game over! Thanks for playing."
+end
+    
